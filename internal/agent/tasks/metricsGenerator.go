5d7ed27194880d74223eed20fcc35d49fc72cc6d
Евгений Шипко <e.shipko@ritm.media>
2025-08-03 00:50:22 +0700

    graceful shutdown for agent and server

--- internal/agent/tasks/metricsGenerator.go
+++ internal/agent/tasks/metricsGenerator.go
@@ -1,29 +1,44 @@
 package tasks
 
 import (
+	"context"
+	"github.com/evgenyshipko/golang-metrics-collector/internal/common/logger"
 	"time"
 
 	"github.com/evgenyshipko/golang-metrics-collector/internal/agent/collector"
 	"github.com/evgenyshipko/golang-metrics-collector/internal/agent/types"
 )
 
-func MetricsGenerator(interval time.Duration, inputCh chan<- types.MetricMessage) {
+func MetricsGenerator(ctx context.Context, interval time.Duration, inputCh chan<- types.MetricMessage) {
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 
 	collectMetricsMemoized := collector.Create()
 
-	for range ticker.C {
-		metrics := *collectMetricsMemoized()
+	for {
+		select {
+		case <-ctx.Done(): // Получен сигнал завершения
+			logger.Instance.Debug("MetricsGenerator between ticker <-ctx.Done()")
+			return
+		case <-ticker.C:
+			metrics := *collectMetricsMemoized()
 
-		for name, value := range metrics {
-			inputCh <- types.MetricMessage{
-				Data: types.MetricValue{
-					Type:  value.Type,
-					Name:  name,
-					Value: value.Value,
-				},
-				Err: nil,
+			for name, value := range metrics {
+				// Проверяем контекст перед отправкой каждой метрики
+				select {
+				case <-ctx.Done():
+					logger.Instance.Debug("MetricsGenerator before sending <-ctx.Done()")
+					return
+				case inputCh <- types.MetricMessage{
+					Data: types.MetricValue{
+						Type:  value.Type,
+						Name:  name,
+						Value: value.Value,
+					},
+					Err: nil,
+				}:
+					// Метрика успешно отправлена
+				}
 			}
 		}
 	}
--- internal/agent/tasks/send.go
+++ internal/agent/tasks/send.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -11,7 +12,7 @@ import (
 	"github.com/evgenyshipko/golang-metrics-collector/internal/common/logger"
 )
 
-func SendMetricsTask(cfg setup.AgentStartupValues, dataChan <-chan types.MetricMessage, errChan chan<- error) {
+func SendMetricsTask(ctx context.Context, cfg setup.AgentStartupValues, dataChan <-chan types.MetricMessage, errChan chan<- error) {
 	ticker := time.NewTicker(cfg.ReportInterval)
 
 	requester := requests.NewRequester(cfg)
@@ -20,35 +21,100 @@ func SendMetricsTask(cfg setup.AgentStartupValues, dataChan <-chan types.MetricM
 
 	for w := 1; w <= cfg.RateLimit; w++ {
 		wg.Add(1)
-		go worker(w, dataChan, errChan, requester, ticker, &wg)
+		go worker(ctx, w, dataChan, errChan, requester, ticker, &wg)
 	}
 
 	go func() {
-		wg.Wait()     // Ждём завершения всех горутин
+		<-ctx.Done()
+		logger.Instance.Debug("SendMetricsTask <-ctx.Done()")
 		ticker.Stop() // останавливаем тикер
 	}()
+
+	wg.Wait() // Ждём завершения всех горутин
 }
 
-func worker(id int, jobs <-chan types.MetricMessage, errChan chan<- error, requester *requests.Requester, ticker *time.Ticker, wg *sync.WaitGroup) {
+/*
+Почему несколько проверок ctx.Done()?
+1. Основная проверка в начале цикла
+go
+case <-ctx.Done():
+
+	return
+
+Зачем: Это "быстрый выход" - если контекст уже завершён, мы сразу выходим, не дожидаясь тикера или новых заданий.
+
+2. Проверка при ожидании нового задания
+go
+case <-ctx.Done():
+
+	return
+
+Зачем: Когда мы ждём новое задание из канала jobs, операция чтения может блокироваться. Мы хотим иметь возможность прервать это ожидание, если пришёл сигнал завершения.
+
+3. Проверка при отправке ошибки
+go
+select {
+case errChan <- err:
+case <-ctx.Done():
+
+	    return
+	}
+
+Зачем: Отправка в канал errChan тоже может блокироваться (если канал заполнен). Мы не хотим зависнуть в этой операции, если программа завершается.
+
+Глубокое объяснение необходимости:
+Блокирующие операции:
+
+Чтение из канала (<-jobs)
+
+Отправка в канал (errChan <- err)
+
+Ожидание тикера (<-ticker.C)
+Все эти операции могут блокировать выполнение
+
+Отзывчивость при завершении:
+
+# Хотим реагировать на завершение максимально быстро
+
+Нельзя допустить, чтобы воркер "завис" в блокирующей операции
+
+Ресурсы и утечки:
+
+# Если воркер не завершится вовремя, это может привести к утечкам
+
+Особенно важно в долгоживущих приложениях
+*/
+func worker(ctx context.Context, id int, jobs <-chan types.MetricMessage, errChan chan<- error, requester *requests.Requester,
+	ticker *time.Ticker, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	fmt.Printf("Worker %d starting\n", id)
-	for range ticker.C {
-		fmt.Printf("Worker %d processing metrics...\n", id)
-		for job := range jobs {
-			if job.Err != nil {
-				logger.Instance.Warnw("Обработка ошибки", "error", job.Err)
-				continue
+	for {
+		select {
+		case <-ctx.Done(): // Получен сигнал завершения
+			return
+		case <-ticker.C:
+			select {
+			case job, ok := <-jobs:
+				if !ok { // Канал закрыт
+					return
+				}
+				if job.Err != nil {
+					logger.Instance.Warnw("Обработка ошибки", "error", job.Err)
+					continue
+				}
+
+				err := requester.SendMetric(job.Data.Type, job.Data.Name, job.Data.Value)
+				if err != nil {
+					select {
+					case errChan <- err:
+					case <-ctx.Done():
+						return
+					}
+				}
+			case <-ctx.Done():
+				return
 			}
-
-			logger.Instance.Infow("worker", "рабочий", id, "запущена задача", job)
-
-			err := requester.SendMetric(job.Data.Type, job.Data.Name, job.Data.Value)
-			if err != nil {
-				errChan <- err
-			}
-
-			logger.Instance.Infow("worker", "рабочий", id, "закончил задачy", job)
 		}
 	}
 }
